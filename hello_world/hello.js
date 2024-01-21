8f4e6d2862de41e9bf76c01665aa4737e65a18b2
zenlex <erichrkeil@gmail.com>
2024-01-21 16:35:09 -0600

    step 6 - connect app to db container

--- hello_world/hello.js
+++ hello_world/hello.js
@@ -1,7 +1,16 @@
 import chalk from 'chalk'
+import pg from 'pg';
 
-function main() {
-	console.log(chalk.blue("Hello World In blue!"));
+async function main() {
+	const client = new pg.Client({
+		connectionString: `postgres://postgres:password@${process.env.DB_HOST}:5432/postgres`
+	});
+	await client.connect();	
+
+	const res = await client.query(`SELECT message from messages ORDER BY random() LIMIT 1`)
+	const msg = res.rows[0].message
+	console.log(chalk.blue(msg));
+	process.exit();
 }
 
-main();
+await main();
